bdef34b30299e0afca5cd43d126476dbd81a71d7
Jotaoesee <jadiazm1995@gmail.com>
2025-01-30 19:58:24 +0100

    Genero cajas en posiciones aleatorias

--- lib/cajapocalipsis.dart
+++ lib/cajapocalipsis.dart
@@ -1,11 +1,14 @@
+import 'dart:math';
 import 'package:flame/components.dart';
 import 'package:flame_forge2d/forge2d_game.dart';
-
 import 'componentes/suelo.dart';
+import 'componentes/caja.dart';
 
 /// Clase principal del juego
 class Cajapocalipsis extends Forge2DGame {
   Cajapocalipsis() : super(gravity: Vector2(0, 10));
+
+  final Random random = Random();
 
   @override
   Future<void> onLoad() async {
@@ -14,5 +17,14 @@ class Cajapocalipsis extends Forge2DGame {
     // Agregamos el suelo
     final suelo = Suelo(Vector2(size.x, 10));
     add(suelo);
+
+    // Generamos cajas en posiciones aleatorias
+    for (int i = 0; i < 5; i++) {
+      final double x = random.nextDouble() * (size.x - 10) + 5; // Evitar bordes
+      final double y =
+          random.nextDouble() * (size.y / 3); // Posición en el aire
+
+      add(Caja(Vector2(x, y), Vector2(4, 4)));
+    }
   }
 }
