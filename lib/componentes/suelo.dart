0ab06292b6919e8a0830596d72bc29a5ac9eee7a
Jotaoesee <jadiazm1995@gmail.com>
2025-01-31 17:31:48 +0100

    añado "renderBody = false" para que no salga visualmente el suelo

--- lib/componentes/suelo.dart
+++ lib/componentes/suelo.dart
@@ -1,28 +1,30 @@
-import 'package:flame/components.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
 
 /// Suelo del juego donde caerán los objetos
 class Suelo extends BodyComponent {
   final Vector2 tamanio;
 
-  Suelo(this.tamanio);
+  Suelo(this.tamanio) {
+    renderBody = false; //  Hace que el suelo NO se renderice visualmente
+  }
 
   @override
-  Future<void> onLoad() async {
-    super.onLoad();
-
-    final forma = PolygonShape()..setAsBoxXY(tamanio.x / 2, tamanio.y / 2);
-
+  Body createBody() {
     final definicionCuerpo = BodyDef(
-      position: Vector2(tamanio.x / 2, tamanio.y + 720), //  Ajustar más abajo
+      position:
+          Vector2(tamanio.x / 2, tamanio.y + 720), // Mantener ajuste manual
       type: BodyType.static,
     );
 
+    final body = world.createBody(definicionCuerpo);
+
+    final forma = PolygonShape()..setAsBoxXY(tamanio.x / 2, tamanio.y / 2);
     final definicionFixture = FixtureDef(forma)
       ..density = 1.0
       ..friction = 0.5
       ..restitution = 0.1;
 
-    body = world.createBody(definicionCuerpo)..createFixture(definicionFixture);
+    body.createFixture(definicionFixture);
+    return body;
   }
 }
